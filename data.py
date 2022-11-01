b27b59400172608ce70c4af6f4bc65a716856b2d
helenagao14 <helena.gao14@gmail.com>
2022-10-31 23:05:15 -0500

    preprocessing text data

--- data.py
+++ data.py
@@ -7,18 +7,20 @@ my_user_agent = 'cs410_movie_nlp'
 
 reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
 
-#extract inital reddit posts / comments for movie reviews
+# extract initial reddit posts / comments for movie reviews
 posts = []
 ml_subreddit = reddit.subreddit('moviecritic')
-for post in ml_subreddit.hot(limit = 1000):
-    post.upvote
-    text = []
-    text.append(post.selftext)
-    submission = reddit.submission(id=post.id)
+for post in ml_subreddit.hot(limit=1000):
+    posts.append([post.title, post.id, post.selftext, post.score, post.upvote_ratio, post.created])
+print('finished posts')
+comments = []
+for post in posts:
+    submission = reddit.submission(post[1])
     submission.comments.replace_more(limit=0)
     for comment in submission.comments.list():
-        text.append(comment)
-    
-    posts.append([post.title, post.id, text, post.score, post.upvote_ratio, post.created])
-df = pd.DataFrame(posts,columns=['title', 'id', 'body', 'score', 'upvote_ratio', 'created'])
+        body = comment.body.replace('\u200b', '')
+        comments.append([post[0], comment.id, body, comment.score, 0, comment.created_utc])
+posts = posts + comments
+print('finished comments')
+df = pd.DataFrame(posts, columns=['title', 'id', 'body', 'score', 'upvote_ratio', 'created'])
 df.to_csv('reddit_scraped_movie_comments.csv')
---
+++ preprocess.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+import string
+import nltk
+from nltk.stem.porter import *
+
+stemmer = PorterStemmer()
+import string
+
+string.punctuation
+# read input file
+data = pd.read_csv('reddit_scraped_movie_comments.csv')
+
+# remove punctuation, numbers, special characters
+data["body"] = data['body'].str.replace("[^a-zA-Z#]", " ")
+data["title_cleaned"] = data['title'].str.replace("[^a-zA-Z#]", " ")
+
+# https://python.plainenglish.io/text-preprocessing-in-natural-language-processing-in-python-8aeb7bfdaee7
+data["body"] = data['body'].str.replace("https?://[^\s\n\r]+", " ")
+data["title_cleaned"] = data['title_cleaned'].str.replace("https?://[^\s\n\r]+", " ")
+
+data["body"] = data['body'].str.replace("[0-9]+", " ")
+data["title_cleaned"] = data['title_cleaned'].str.replace("[0-9]+", " ")
+
+# lowercase
+for index, row in data.iterrows():
+    row['body'] = (str(row['body']).lower())
+    row['title_cleaned'] = (str(row['title_cleaned']).lower())
+    # remove short words
+    row["body"] = ' '.join([w for w in row['body'].split() if len(w) > 2])
+    row["title_cleaned"] = ' '.join([w for w in row['title_cleaned'].split() if len(w) > 2])
+
+    # stemmer
+    row['body'] = ' '.join([stemmer.stem(w) for w in row['body'].split()])
+    row['title_cleaned'] = ' '.join([stemmer.stem(w) for w in row['title_cleaned'].split()])
+
+    # lemmatizer
+
+    data['body'] = row['body']
+    data['title_cleaned'] = row['title_cleaned']
+
+#
+# data.dropna(subset=["text"], inplace=True)
+
+data.to_csv(index=False, path_or_buf="reddit_data/cleaned_reddit_comments.csv")
