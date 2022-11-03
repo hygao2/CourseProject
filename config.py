f68c81a9d354641c05485a2ed5d903d87b623315
helenagao14 <helena.gao14@gmail.com>
2022-11-02 19:31:08 -0500

    moved secret key api access to config file

---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+reddit = {
+    "my_client_id": "dkmj5CVNjnzx2esWgeViEg",
+    "my_client_secret": "6Hh3UEdpqmXdMqi03sLYDhCSYJOYZA",
+    "my_user_agent": "cs410_movie_nlp"
+}
--- data.py
+++ data.py
@@ -1,11 +1,10 @@
 import praw
 import pandas as pd
+from config import *
 
-my_client_id = 'dkmj5CVNjnzx2esWgeViEg'
-my_client_secret = '6Hh3UEdpqmXdMqi03sLYDhCSYJOYZA'
-my_user_agent = 'cs410_movie_nlp'
-
-reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
+reddit = praw.Reddit(client_id=reddit['my_client_id'],
+                     client_secret=reddit['my_client_secret'],
+                     user_agent=reddit['my_user_agent'])
 
 def movie_comments(movie, subreddit, limit):
     list = []
@@ -22,25 +21,9 @@ combined_string = sub1 + "+" + sub2
 combined_sub = reddit.subreddit(combined_string)
 
 movie = "The Dark Knight"
-limit = 10
+limit = 5
 
 comments = movie_comments(movie, combined_sub, limit)
-
-# extract initial reddit posts / comments for movie reviews
-# posts = []
-# ml_subreddit = reddit.subreddit('moviecritic')
-# for post in ml_subreddit.hot(limit=1000):
-#     posts.append([post.title, post.id, post.selftext, post.score, post.upvote_ratio, post.created])
-# print('finished posts')
-# comments = []
-#
-# for post in posts:
-#     submission = reddit.submission(post[1])
-#     submission.comments.replace_more(limit=0)
-#     for comment in submission.comments.list():
-#         body = comment.body.replace('\u200b', '')
-#         comments.append([post[0], comment.id, body, comment.score, 0, comment.created_utc])
-# posts = posts + comments
 print('finished comments')
 
 df = pd.DataFrame(comments, columns=['title', 'id', 'body', 'score', 'created'])
