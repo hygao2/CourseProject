cd872c463265352adb97339f3bd5ca540b0869a6
helenagao14 <helena.gao14@gmail.com>
2022-11-02 19:20:35 -0500

    sentiment scores

--- data.py
+++ data.py
@@ -7,20 +7,41 @@ my_user_agent = 'cs410_movie_nlp'
 
 reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
 
+def movie_comments(movie, subreddit, limit):
+    list = []
+    for post in subreddit.search(movie, limit=limit):
+        post.comments.replace_more(limit=0)
+        all_comments = post.comments.list()
+        for comment in all_comments:
+            list.append([comment.id, comment.submission.title, comment.body, comment.score, comment.created_utc])
+    return list
+
+sub1 = "movies"
+sub2 = "MovieDetails"
+combined_string = sub1 + "+" + sub2
+combined_sub = reddit.subreddit(combined_string)
+
+movie = "The Dark Knight"
+limit = 10
+
+comments = movie_comments(movie, combined_sub, limit)
+
 # extract initial reddit posts / comments for movie reviews
-posts = []
-ml_subreddit = reddit.subreddit('moviecritic')
-for post in ml_subreddit.hot(limit=1000):
-    posts.append([post.title, post.id, post.selftext, post.score, post.upvote_ratio, post.created])
-print('finished posts')
-comments = []
-for post in posts:
-    submission = reddit.submission(post[1])
-    submission.comments.replace_more(limit=0)
-    for comment in submission.comments.list():
-        body = comment.body.replace('\u200b', '')
-        comments.append([post[0], comment.id, body, comment.score, 0, comment.created_utc])
-posts = posts + comments
+# posts = []
+# ml_subreddit = reddit.subreddit('moviecritic')
+# for post in ml_subreddit.hot(limit=1000):
+#     posts.append([post.title, post.id, post.selftext, post.score, post.upvote_ratio, post.created])
+# print('finished posts')
+# comments = []
+#
+# for post in posts:
+#     submission = reddit.submission(post[1])
+#     submission.comments.replace_more(limit=0)
+#     for comment in submission.comments.list():
+#         body = comment.body.replace('\u200b', '')
+#         comments.append([post[0], comment.id, body, comment.score, 0, comment.created_utc])
+# posts = posts + comments
 print('finished comments')
-df = pd.DataFrame(posts, columns=['title', 'id', 'body', 'score', 'upvote_ratio', 'created'])
-df.to_csv('reddit_scraped_movie_comments.csv')
+
+df = pd.DataFrame(comments, columns=['title', 'id', 'body', 'score', 'created'])
+df.to_csv('reddit_data/reddit_scraped_movie_comments.csv')
--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,7 @@ sw_nltk = stopwords.words('english')
 sw_nltk.remove('not')
 
 from nltk.sentiment import SentimentIntensityAnalyzer
+
 sia = SentimentIntensityAnalyzer()
 
 # Init the Wordnet Lemmatizer
@@ -20,48 +21,33 @@ import string
 
 string.punctuation
 # read input file
-data = pd.read_csv('reddit_scraped_movie_comments.csv')
+data = pd.read_csv('reddit_data/reddit_scraped_movie_comments.csv')
 
 # remove punctuation, numbers, special characters
 # https://python.plainenglish.io/text-preprocessing-in-natural-language-processing-in-python-8aeb7bfdaee7
 data["body"] = data['body'].str.replace("https?://[^\s\n\r]+", " ")
-data['title_cleaned'] = data['title'].str.replace("https?://[^\s\n\r]+", " ")
-
 data["body"] = data['body'].str.replace("[^a-zA-Z#]", " ")
-data["title_cleaned"] = data['title_cleaned'].str.replace("[^a-zA-Z#]", " ")
-
 data["body"] = data['body'].str.replace("[0-9]+", " ")
-data["title_cleaned"] = data['title_cleaned'].str.replace("[0-9]+", " ")
 
-data['body_polarity'] = 0
-data['title_polarity'] = 0
 # lowercase
 for index, row in data.iterrows():
     row['body'] = (str(row['body']).lower())
-    row['title_cleaned'] = (str(row['title_cleaned']).lower())
     # remove short words
     row["body"] = ' '.join([w for w in row['body'].split() if len(w) > 2])
-    row["title_cleaned"] = ' '.join([w for w in row['title_cleaned'].split() if len(w) > 2])
 
-    # lemmatizer
+    # lemmatize
     # https://www.machinelearningplus.com/nlp/lemmatization-examples-python/
     row['body'] = ' '.join([lemmatizer.lemmatize(w) for w in row['body'].split()])
-    row['title_cleaned'] = ' '.join([lemmatizer.lemmatize(w) for w in row['title_cleaned'].split()])
 
     # remove stop words
     # https://towardsdatascience.com/text-pre-processing-stop-words-removal-using-different-libraries-f20bac19929a
     row['body'] = ' '.join([word for word in row['body'].split() if word.lower() not in sw_nltk])
-    row['title_cleaned'] = ' '.join([word for word in row['title_cleaned'].split() if word.lower() not in sw_nltk])
 
-
-
-    if len(row['body']) != 0:
-        data.at[index, 'body'] = row['body']
-        data.at[index, 'body_polarity'] = sia.polarity_scores(row['body'])['compound']
-    if len(row['title_cleaned']) != 0:
-        data.at[index, 'title_cleaned'] = row['title_cleaned']
-
-#
-# data.dropna(subset=["text"], inplace=True)
+    data.at[index, 'body'] = row['body']
+    # sentiment scores
+    data.at[index, 'neg'] = sia.polarity_scores(row['body'])['neg']
+    data.at[index, 'neu'] = sia.polarity_scores(row['body'])['neu']
+    data.at[index, 'pos'] = sia.polarity_scores(row['body'])['pos']
+    data.at[index, 'compound'] = sia.polarity_scores(row['body'])['compound']
 
 data.to_csv(index=False, path_or_buf="reddit_data/cleaned_reddit_comments.csv")
