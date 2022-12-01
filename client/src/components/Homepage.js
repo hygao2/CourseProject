4495af5a468a9a28b2c337f2e3e32714b48db695
gracejj2 <gracejj2@illinois.edu>
2022-11-30 22:40:14 -0600

    Added top 5 comments, chart skeleton, sentiment analysis

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
 import "./Homepage.css";
 
 function Homepage() {
@@ -7,9 +9,28 @@ function Homepage() {
     movie_name: "",
     movie_sentiment: 0,
     rating: 0,
+    top_five_comments: [],
+    sentiment_classification: "",
   });
   const [isSearchSuccessful, setIsSearchSuccessful] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [options, setOptions] = useState({
+    series: [
+      {
+        name: "sentiment",
+        data: [30, 40, 45, 50, 49],
+      },
+    ],
+    xaxis: {
+      categories: [
+        "mon aug 6",
+        1668863278.0,
+        1668865268.0,
+        1668866268.0,
+        1668963268.0,
+      ],
+    },
+  });
 
   const searchHandler = (event) => {
     event.preventDefault();
@@ -20,12 +41,13 @@ function Homepage() {
       res.json().then((data) => {
         console.log(data);
 
-        // Setting a data from api
         setIsLoading(false);
         setResult({
           movie_name: data.movie_name,
           movie_sentiment: data.movie_sentiment,
           rating: data.rating,
+          top_five_comments: data.top_five_comments,
+          sentiment_classification: data.sentiment_classification,
         });
         setIsSearchSuccessful(true);
       })
@@ -35,6 +57,7 @@ function Homepage() {
   return (
     <div className="wrapper">
       <div className="header">Movie Sentiment Analysis</div>
+      
       <form>
         <label htmlFor="name_input">Enter a movie name: </label>
         <input
@@ -47,13 +70,32 @@ function Homepage() {
         </button>
       </form>
 
-      {isLoading && <div>Loading...</div>}
+      {isLoading && <div className="loading">Loading...</div>}
 
       {isSearchSuccessful && (
         <div className="results">
           <p>You searched for: {result.movie_name}</p>
           <p>This movie has an IMDB rating of: {result.rating}</p>
-          <p>This movie has an average sentiment of: {result.movie_sentiment}</p>
+          <p>
+            This movie has an average sentiment of: {result.movie_sentiment}{" "}
+            which is a mostly {result.sentiment_classification} reaction.
+          </p>
+
+          <h3>Top 5 Sentiment Comments from Reddit:</h3>
+          {result.top_five_comments.map((comment, index) => (
+            <p>
+              {index + 1}. {comment}
+            </p>
+          ))}
+
+          <h3>Sentiment Chart Over Time</h3>
+          <Chart
+            className="chart"
+            options={options}
+            series={options.series}
+            type="line"
+            width="500"
+          />
         </div>
       )}
     </div>
--- flask-server/server.py
+++ flask-server/server.py
@@ -13,21 +13,42 @@ def returnRating(name):
     df = pd.read_csv("IMDbRating.csv")
     return {"rating": df.loc[df['Title'] == name]['Rating'].to_string()[-3:]}
   
-# Preprocess movie comments and return movie name
+# Preprocess movie comments and return movie data
 @app.route('/movie/<name>')
 def get_movie(name):
     comments = movie_comments(name)
     df = pd.DataFrame(comments, columns=['title', 'id', 'body', 'score', 'created'])
     df.to_csv('../reddit_data/reddit_scraped_movie_comments.csv')
+    
+    # Sentiment Calculation
     preprocess()
     avg_sentiment = calculate_avg_sentiment()
+    sentiment_classification = "neutral"
+    if avg_sentiment <-0.03:
+        sentiment_classification = "negative"
+    elif avg_sentiment < 0.03:
+        sentiment_classification = "neutral"
+    else:
+        sentiment_classification = "positive"
 
+    # Top five sentiment comments
+    cleaned_df = pd.read_csv('../reddit_data/cleaned_reddit_comments.csv')
+    sorted_df = cleaned_df.sort_values(by=['compound'], ascending=False)
+    top_five_indices = sorted_df.head(5)['Unnamed: 0']
+    top_five_comments = []
+    for index in top_five_indices:
+        top_five_comments.append(df.iloc[int(index)]['body'])
+
+    # IMDB rating
     imdb_df = pd.read_csv("IMDbRating.csv")
     rating = imdb_df.loc[imdb_df['Title'] == name]['Rating'].to_string()[-3:]
 
     # Returning an api for showing in  reactjs
-    return {"movie_name" : name, "movie_sentiment" : avg_sentiment, "rating" : rating}
-
+    return {"movie_name" : name, 
+            "movie_sentiment" : avg_sentiment, 
+            "rating" : rating,
+            "top_five_comments" : top_five_comments,
+            "sentiment_classification" : sentiment_classification}
   
 # Running app
 if __name__ == '__main__':
